d87c785f6db9680b8628cd8adbd8ee8fedea3d27
Cypher14x <alexcoro14x@gmail.com>
2025-08-08 18:17:59 -0500

    prueba

---
+++ src/generics/generics.ts
@@ -0,0 +1,8 @@
+export const printObjetc = (argument:any) => {
+    console.log(argument);
+    
+}
+
+export function genericFunction(argument: any): any {
+    return argument;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,38 @@
 
-import {Hero as Heroe} from './classes/Hero';
+// import {Hero as Heroe} from './classes/Hero';
 
-// import * as HeroeClass from './classes/Hero'; //importar todo lo de Hero como HeroeClass
+import { printObjetc, genericFunction} from "./generics/generics";
 
-
-// import  powers2  from './data/powers'; 
-
-//suponiendo que Hero es una variable que no puedo cambiar el nombre, para no tener conflictos con la clase Hero
-//renombramos Hero a Heroe
-// const Hero = 123;
-
-console.log('Hola Mundo!');
-
-const ironman = new Heroe('Ironman', 6, 45);
-
-console.log(ironman.power);
-
-// console.log(powers2);
+// // import * as HeroeClass from './classes/Hero'; //importar todo lo de Hero como HeroeClass
 
 
+// // import  powers2  from './data/powers'; 
+
+// //suponiendo que Hero es una variable que no puedo cambiar el nombre, para no tener conflictos con la clase Hero
+// //renombramos Hero a Heroe
+// // const Hero = 123;
+
+// console.log('Hola Mundo!');
+
+// const ironman = new Heroe('Ironman', 6, 45);
+
+// console.log(ironman.power);
+
+// // console.log(powers2);
 
 
 
 
+//manejando genericos
+
+printObjetc('Hola Mundo!');
+printObjetc(123);
+printObjetc([1, 2, 3, 4, 5]);
+printObjetc({ name: 'Ironman', power: 6, age: 45 });
+
+
+console.log(genericFunction('Hola Mundo!').toFixed(2)); // Esto causará un error porque toFixed no es un método de string);
+console.log(genericFunction(3.1416).toFixed(2));
+ 
 
 
