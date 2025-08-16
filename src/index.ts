c98c445ceb66303ee77fb91fa262fd43858bbdd7
Cypher14x <alexcoro14x@gmail.com>
2025-08-16 12:38:40 -0500

    ejemplo generics con pokeapi y axios

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { getPokemon } from "./generics/get-pokemon";
 
+
+getPokemon(1)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+    .finally(() => console.log('Finalizó la ejecución'));
