a20c7b0cf286062b0cdff5259f83cf866eb65548
Alex <alexcoro14xd@gmail.com>
2025-08-19 23:23:53 -0500

    decoradores de clase y factory decorators

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -3,7 +3,16 @@ function printoConsole(constructor: Function) {
     console.log(constructor);
 }
 
-@printoConsole
+const printToConsoleConditional = (print:boolean = false): Function => {
+    if(print) {
+        return printoConsole;
+    }else{
+        return () => {};
+    }
+    
+}
+
+@printToConsoleConditional(true)
 export class Pokemon {
 
     public publicApi: string = 'https://pokeapi.co';
